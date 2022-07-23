49a21e3179ae05d0888e4b0266c9baae988d1459
Geraldine <geraldine.orellana@gmail.com>
2022-07-23 19:28:24 -0400

    EventDetails created

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="event-card"> 
+  <span>@ {{ event.time }} on {{ event.date }} </span>
+  <h4>{{ event.title }} </h4>
+  </div> 
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            event: null
+        }
+    },
+    created (){
+        // fetch event (by id) and set local data
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -10,6 +10,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js';
 
 export default {
   name: 'EventList',
@@ -18,47 +19,24 @@ export default {
   },
 
    data() {
-        return {
-            events: [
-            {
-                id: 5928101,
-                category: 'animal welfare',
-                title: 'Cat Adoption Day',
-                description: 'Find your new feline friend',
-                location: 'Miau Town',
-                date: 'Jan 1st, 2022',
-                time: '12:00',
-                petsAllowed: true,
-                organizer: 'Kat Laydee'
-            },
-            {
-                id: 4582797,
-                category: 'food',
-                title: 'Community Garding',
-                description: 'Join us',
-                location: 'Plant Town',
-                date: 'Jan 2nd, 2022',
-                time: '12:00',
-                petsAllowed: true,
-                organizer: 'Fern Poline'
-
-            },
-            {
-                id: 2547697,
-                category: 'sustainability',
-                title: 'Beach Cleanup',
-                description: 'Help us cleaning up',
-                location: 'Playa El Tabo',
-                date: 'Jan 3rd, 2022',
-                time: '12:00',
-                petsAllowed: false,
-                organizer: 'Carey Wales'
-            }
-            
-            ]
+      return {
+         events: null
 
         }
+    },
+    created(){
+     
+     EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+
     }
+
 }
 </script>
 <style scoped>
